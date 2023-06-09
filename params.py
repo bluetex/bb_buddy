9631098cd199d3ac2046f2e78e18b0d51ad32a67
bluetex <newton.jeff.t@gmail.com>
2023-06-09 18:48:02 -0500

    update setup breakout homedir

--- params.py
+++ params.py
@@ -1,4 +1,6 @@
 import mido
 port_name = 'AudioBox iTwo MIDI In 6'
 outport = 'AudioBox iTwo MIDI Out 8'
-homedir_path = r'D:\beatbuddy\Complete Collection\Premium Library Complete Collection 2023\SONGS'
+homedir_base = r'D:\beatbuddy\Complete Collection\Premium Library Complete Collection 2023'
+homedir_songs = r'D:\beatbuddy\Complete Collection\Premium Library Complete Collection 2023\SONGS'
+homedir_drumsets = r'D:\beatbuddy\Complete Collection\Premium Library Complete Collection 2023\DRUMSETS'
--- start.py
+++ start.py
@@ -5,7 +5,7 @@ import os
 import mido
 import subprocess
 import webbrowser
-from params import port_name, outport, homedir_path as default_homedir_path
+from params import port_name, outport, homedir_base as default_homedir
 
 app = Flask(__name__)
 
@@ -20,13 +20,13 @@ def validate_params():
     if port_name not in input_names or outport not in output_names:
         return False
 
-    if not os.path.isdir(default_homedir_path):
+    if not os.path.isdir(default_homedir):
         return False
 
     return True
 
-def write_params(selected_input, selected_output, custom_homedir_path):
-    params_code = f"import mido\nport_name = '{selected_input}'\noutport = '{selected_output}'\nhomedir_path = r'{custom_homedir_path}'"
+def write_params(selected_input, selected_output, custom_homedir):
+    params_code = f"import mido\nport_name = '{selected_input}'\noutport = '{selected_output}'\nhomedir_base = r'{custom_homedir}'\nhomedir_songs = r'{custom_homedir}\\SONGS'\nhomedir_drumsets = r'{custom_homedir}\\DRUMSETS'"
     with open('params.py', 'w') as file:
         file.write(params_code)
 
@@ -35,15 +35,15 @@ def index():
     if request.method == 'POST':
         selected_input = request.form.get('input')
         selected_output = request.form.get('output')
-        custom_homedir_path = request.form.get('homedir_path')
-        write_params(selected_input, selected_output, custom_homedir_path)
+        custom_homedir = request.form.get('homedir_base')
+        write_params(selected_input, selected_output, custom_homedir)
         # Modify the redirect below to the desired endpoint or route
         
         subprocess.Popen(['python', 'app.py'])
         return redirect('http://localhost:5000/')
 
     if not validate_params():
-        return render_template('setup.html', inputs=inz, outputs=outz, homepath=default_homedir_path)
+        return render_template('setup.html', inputs=inz, outputs=outz, homepath=default_homedir)
     
     subprocess.Popen(['python', 'app.py'])
     return redirect('http://localhost:5000/')        
